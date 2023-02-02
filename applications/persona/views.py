6e5abd1353414401426334be3ea33a5b4b00e978
jorgeromeroc <jromerocontreras@icloud.com>
2023-02-01 23:58:55 -0300

    Modulo de gestion de empleados terminado.

--- applications/persona/views.py
+++ applications/persona/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import (ListView, DetailView)
+from django.urls import reverse_lazy
+from django.views.generic import (ListView, DetailView, CreateView, TemplateView, UpdateView, DeleteView)
 
 from .models import Empleado
 
@@ -52,3 +53,57 @@ class EmpleadoDetailView(DetailView):
         context = super(EmpleadoDetailView, self).get_context_data(**kwargs)
         context['titulo'] = 'Empleado del mes'
         return context
+
+#==========================================
+# Quiero que cuando se envie el formulario
+# de registro de nuevo empleado, me redirija
+# a  otra pagina
+#==========================================
+class SuccessView(TemplateView):
+    template_name = "persona/success.html"
+
+
+#==========================================
+# Creacion de nuevo empleado
+#==========================================
+class EmpleadoCreateView(CreateView):
+    template_name = "persona/add.html"
+    model = Empleado
+    #podemos solo pedir algunos campos
+    fields = ['first_name', 'last_name', 'job', 'departamento', 'habilidades',]
+
+    #o pedimos todos los campos de ese modelo
+    #fields = ('__all__')
+    #con el uso de "reverse_lazy" para la redireccion
+    # debemos agregar en las urls.py de la app el atributo name
+    success_url = reverse_lazy('persona_app:correcto')
+
+    def form_valid(self, form):
+        empleado = form.save()
+        empleado.full_name = empleado.first_name + ' ' + empleado.last_name
+        empleado.save()
+        #super tambien se utiliza para indicar que voy a sibreecribir
+        #un funcion pre-establecida como en este caso "form_valid"
+        return super(EmpleadoCreateView, self).form_valid(form)
+
+class EmpleadoUpdateView(UpdateView):
+    template_name = "persona/update.html"
+    model = Empleado
+    fields = ['first_name', 'last_name', 'job', 'departamento', 'habilidades', ]
+    success_url = reverse_lazy('persona_app:correcto')
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        print(request.POST)
+        print(request.POST['last_name'])
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        return super(EmpleadoUpdateView, self).form_valid(form)
+
+class EmpleadoDeleteView(DeleteView):
+    model = Empleado
+    template_name = "persona/delete.html"
+    success_url = reverse_lazy('persona_app:correcto')
+
+
